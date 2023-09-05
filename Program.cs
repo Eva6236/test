bcfdb2e42b3c6a03ce6d545db1a376e162dac5ba
Eva Klets <Evaklets@mail.ru>
2023-09-05 21:11:44 +0300

    тестовое задание

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿    
+int n = 30;
+int m = 10;
+int x = 10000;
+int total = calculate (n,m,x);
+System.Console.WriteLine(total);
+
+
+int calculate(int n,int m, int x ) 
+{
+int total = 0;
+int price = x;
+
+for (int i = 1; i <= n; i++)
+{
+    total += price;
+
+    if (i % m == 0)
+    {
+        x += 1000;
+    }  
+    
+}
+    
+    return total;
+
+}
+
+
